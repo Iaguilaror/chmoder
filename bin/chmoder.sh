4e486a7a96c1f6b798ea5f9c73a9fa4974f13726
Israel Aguilar <iaguilaror@gmail.com>
2020-01-29 15:29:07 -0600

    first working version

--- bin/chmoder.sh
+++ bin/chmoder.sh
@@ -31,7 +31,44 @@ fi
 
 ## define desired permissions (its better to use human nomenclature, like g+rw)
 permissions="770"
-echo "[$time_stamp] detecting file in $target_dir with any of the following missing permissions: $permissions" >> $logfile
 
-## find every file without the required permissions
-find $target_dir ! -perm $permissions -type f -exec ls -l {} \;
+## count the number of dirs without the expected permissions
+number_of_dirs=$(find $target_dir ! -perm $permissions -type d | wc -l)
+echo "[$time_stamp] found dirs without the correct permissions: $number_of_dirs" >> $logfile
+
+## test if there are dirs in need of chmodding
+if [[ "$number_of_dirs" -eq 0 ]]
+then
+	echo "[$time_stamp] No dirs need chmodding. Skipping change of permissions for directories" >> $logfile
+else
+	echo -e "[$time_stamp] detecting dirs in $target_dir NOT set to the following permissions: $permissions\n=" >> $logfile
+	## find every dir without the required permissions
+	find $target_dir ! -perm $permissions -type d >> $logfile
+	echo -e "=\n[$time_stamp] modifying permissions on said dirs" >> $logfile
+	## modify permissions in the dirs found
+	find $target_dir ! -perm $permissions -type d -exec chmod $permissions {} \;
+	## message of change
+	echo "[$time_stamp] DETECTED dirs PERMISSIONS HAVE BEEN SET TO: $permissions" >> $logfile 
+fi
+
+## count the number of files without the expected permissions
+number_of_files=$(find $target_dir ! -perm $permissions -type f | wc -l)
+echo "[$time_stamp] found files without the correct permissions: $number_of_files" >> $logfile
+
+## test if there are files in need of chmodding
+if [[ "$number_of_files" -eq 0 ]]
+then
+	echo "[$time_stamp] No files need chmodding. Skipping change of permissions for files" >> $logfile
+else
+	echo -e "[$time_stamp] detecting files in $target_dir NOT set to the following permissions: $permissions\n=" >> $logfile
+	## find every file without the required permissions
+	find $target_dir ! -perm $permissions -type f >> $logfile
+	echo -e "=\n[$time_stamp] modifying permissions on said files" >> $logfile
+	## modify permissions in the files found
+	find $target_dir ! -perm $permissions -type f -exec chmod $permissions {} \;
+	## message of change
+	echo "[$time_stamp] DETECTED files PERMISSIONS HAVE BEEN SET TO: $permissions" >> $logfile
+fi
+
+##end message
+echo "[$time_stamp] ===== END OF CRONJOB" >> $logfile
