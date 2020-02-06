93f924df57b8a13dc8c14f5e4842009bd8a3f43b
Israel Aguilar <iaguilaror@gmail.com>
2020-02-06 11:00:30 -0600

    implement gzip compression

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # chmoder
 Script for changing permissions on every file that does not already have them in a dir
+
+Note:
+Remember to ad the chmoder script in a cronjob, for ubuntu use:
+
+````
+nano /etc/crontab
+````
+
+Example cronjob:
+
+````
+* * * * * root bash /home/iaguilar/tmp/chmoder/bin/chmoder.sh /home/iaguilar/tmp/chmoder/test/data/
+````
+
+### Dependencies
+
+gzip
--- bin/chmoder.sh
+++ bin/chmoder.sh
@@ -6,7 +6,7 @@
 logs_directory=$(dirname "$(readlink -f "$0")")
 
 ## define logfile from logs_directory location
-logfile="$logs_directory"/chmoder_logs/log.txt
+logfile="$logs_directory"/chmoder_logs/log.txt.gz
 
 ## create a dir if needed, relative to the logfile
 # -p option avoids error if dir already exists
@@ -15,6 +15,9 @@ mkdir -p $(dirname "$logfile")
 ## touch the timestamp of the logfile
 # if the files does not exist, it is created
 touch "$logfile"
+
+##end message
+echo "= START OF CRONJOB" | gzip >> $logfile
 
 ## capture the target directory
 target_dir="$1"
@@ -25,7 +28,7 @@ time_stamp=$(date | tr " " "_")
 ## Check, if target dir does NOT exist, the scripts ends abruptly
 if [[ ! -d "$target_dir" ]]
 then
-	echo "[$time_stamp] $target_dir does not exists on your filesystem. chmoder won't do anything. Exit program" >> $logfile
+	echo "[$time_stamp] $target_dir does not exists on your filesystem. chmoder won't do anything. Exit program" | gzip >> $logfile
 	exit 1
 fi
 
@@ -33,42 +36,42 @@ fi
 permissions="770"
 
 ## count the number of dirs without the expected permissions
-number_of_dirs=$(find $target_dir ! -perm $permissions -type d | wc -l)
-echo "[$time_stamp] found dirs without the correct permissions: $number_of_dirs" >> $logfile
+number_of_dirs=$(find $target_dir -mindepth 1 ! -perm $permissions -type d | wc -l)
+echo "[$time_stamp] found dirs without the correct permissions: $number_of_dirs" | gzip >> $logfile
 
 ## test if there are dirs in need of chmodding
 if [[ "$number_of_dirs" -eq 0 ]]
 then
-	echo "[$time_stamp] No dirs need chmodding. Skipping change of permissions for directories" >> $logfile
+	echo "[$time_stamp] No dirs need chmodding. Skipping change of permissions for directories" | gzip >> $logfile
 else
-	echo -e "[$time_stamp] detecting dirs in $target_dir NOT set to the following permissions: $permissions\n=" >> $logfile
+	echo -e "[$time_stamp] detecting dirs in $target_dir NOT set to the following permissions: $permissions\n=" | gzip >> $logfile
 	## find every dir without the required permissions
-	find $target_dir ! -perm $permissions -type d >> $logfile
-	echo -e "=\n[$time_stamp] modifying permissions on said dirs" >> $logfile
+	find $target_dir -mindepth 1 ! -perm $permissions -type d | gzip >> $logfile
+	echo -e "=\n[$time_stamp] modifying permissions on said dirs" | gzip >> $logfile
 	## modify permissions in the dirs found
-	find $target_dir ! -perm $permissions -type d -exec chmod $permissions {} \;
+	find $target_dir -mindepth 1 ! -perm $permissions -type d -exec chmod $permissions {} \;
 	## message of change
-	echo "[$time_stamp] DETECTED dirs PERMISSIONS HAVE BEEN SET TO: $permissions" >> $logfile 
+	echo "[$time_stamp] DETECTED dirs PERMISSIONS HAVE BEEN SET TO: $permissions" | gzip >> $logfile
 fi
 
 ## count the number of files without the expected permissions
 number_of_files=$(find $target_dir ! -perm $permissions -type f | wc -l)
-echo "[$time_stamp] found files without the correct permissions: $number_of_files" >> $logfile
+echo "[$time_stamp] found files without the correct permissions: $number_of_files" | gzip >> $logfile
 
 ## test if there are files in need of chmodding
 if [[ "$number_of_files" -eq 0 ]]
 then
-	echo "[$time_stamp] No files need chmodding. Skipping change of permissions for files" >> $logfile
+	echo "[$time_stamp] No files need chmodding. Skipping change of permissions for files" | gzip >> $logfile
 else
-	echo -e "[$time_stamp] detecting files in $target_dir NOT set to the following permissions: $permissions\n=" >> $logfile
+	echo -e "[$time_stamp] detecting files in $target_dir NOT set to the following permissions: $permissions\n=" | gzip >> $logfile
 	## find every file without the required permissions
-	find $target_dir ! -perm $permissions -type f >> $logfile
-	echo -e "=\n[$time_stamp] modifying permissions on said files" >> $logfile
+	find $target_dir ! -perm $permissions -type f | gzip >> $logfile
+	echo -e "=\n[$time_stamp] modifying permissions on said files" | gzip >> $logfile
 	## modify permissions in the files found
 	find $target_dir ! -perm $permissions -type f -exec chmod $permissions {} \;
 	## message of change
-	echo "[$time_stamp] DETECTED files PERMISSIONS HAVE BEEN SET TO: $permissions" >> $logfile
+	echo "[$time_stamp] DETECTED files PERMISSIONS HAVE BEEN SET TO: $permissions" | gzip >> $logfile
 fi
 
 ##end message
-echo "[$time_stamp] ===== END OF CRONJOB" >> $logfile
+echo "= END OF CRONJOB" | gzip >> $logfile
